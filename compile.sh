6b180c961d097e7ca505fc8841f6642f44b78645
Pedro-Juan Ferrer Matoses <vehrka@gmail.com>
2024-08-12 18:18:45 +0200

    Update folders

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 
 cd ~/Documents/dev/lcdor_export
 rm -fr ~/Documents/dev/lcdor_export/lcdor_export/*
-rm -fr ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/*
-obsidian-export --ignore-file ~/Documents/dev/lcdor_export/.export-ignore --frontmatter=always ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/Worlds/1.\ La\ Caza\ del\ Ojo\ Rojo ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/
-sed -i 's@Worlds/1. La Caza del Ojo Rojo@lcdor_export@' ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/Mapa\ lcdor\ jugadores.md
-find ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\[/[/' {} \;
-find ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\]/]/' {} \;
-find ~/Documents/doc/rpg/obsidian/sessions/rpg_sessions/lcdor_export/ -name "*.md" -exec sed -i -e 's/ > $//' {} \;
+rm -fr ~/Documents/RPG_SESSIONS/lcdor_export/*
+obsidian-export --ignore-file ~/Documents/dev/lcdor_export/.export-ignore --frontmatter=always ~/Documents/RPG_SESSIONS/Worlds/eau\ -\ La\ Caza\ del\ Ojo\ Rojo/ ~/Documents/RPG_SESSIONS/lcdor_export/
+sed -i 's@Worlds/eau - La Caza del Ojo Rojo@lcdor_export@' ~/Documents/RPG_SESSIONS/lcdor_export/Mapa\ lcdor\ jugadores.md
+find ~/Documents/RPG_SESSIONS/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\[/[/' {} \;
+find ~/Documents/RPG_SESSIONS/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\]/]/' {} \;
+find ~/Documents/RPG_SESSIONS/lcdor_export/ -name "*.md" -exec sed -i -e 's/ > $//' {} \;
 

04d4d6e89a867ebf8f3b9bda692bf5e230bb5a5e
Pedro-Juan Ferrer Matoses <vehrka@gmail.com>
2024-08-12 19:14:43 +0200

    S46 Update

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 cd ~/Documents/dev/lcdor_export
 rm -fr ~/Documents/dev/lcdor_export/lcdor_export/*
 rm -fr ~/Documents/RPG_SESSIONS/lcdor_export/*
-obsidian-export --ignore-file ~/Documents/dev/lcdor_export/.export-ignore --frontmatter=always ~/Documents/RPG_SESSIONS/Worlds/eau\ -\ La\ Caza\ del\ Ojo\ Rojo/ ~/Documents/RPG_SESSIONS/lcdor_export/
+obsidian-export --ignore-file ~/Documents/dev/lcdor_export/.export-ignore --frontmatter=always ~/Documents/RPG_SESSIONS/Worlds/eau\ -\ La\ Caza\ del\ Ojo\ Rojo ~/Documents/RPG_SESSIONS/lcdor_export
 sed -i 's@Worlds/eau - La Caza del Ojo Rojo@lcdor_export@' ~/Documents/RPG_SESSIONS/lcdor_export/Mapa\ lcdor\ jugadores.md
 find ~/Documents/RPG_SESSIONS/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\[/[/' {} \;
 find ~/Documents/RPG_SESSIONS/lcdor_export/ -name "*.md" -exec sed -i -e 's/\\\]/]/' {} \;
